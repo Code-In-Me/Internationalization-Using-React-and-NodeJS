db88a622cd5b9ef52a619bc820a4002bff27d5ad
Code-In-Me <60462963+Code-In-Me@users.noreply.github.com>
2020-11-23 07:25:47 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json())
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({"message": "Welcome "});
 });
 
 require('./app/routes/language.routes.js')(app);
