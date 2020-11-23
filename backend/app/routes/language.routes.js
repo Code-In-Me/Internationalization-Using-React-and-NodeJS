2b2c656facb84fbdc98704cc59ed6a3c14370ce8
Code-In-Me <60462963+Code-In-Me@users.noreply.github.com>
2020-11-23 07:27:15 -0800

    Update language.routes.js

--- backend/app/routes/language.routes.js
+++ backend/app/routes/language.routes.js
@@ -7,12 +7,5 @@ module.exports = (app) => {
     // Retrieve all languages
     app.get('/languages', languages.findAll);
 
-    // Retrieve a single Note with noteId
-    app.get('/languages/:noteId', languages.findOne);
-
-    // Update a Note with noteId
-    app.put('/languages/:noteId', languages.update);
-
-    // Delete a Note with noteId
-    app.delete('/languages/:noteId', languages.delete);
+   
 }
